00b92cd1a7681918990039ef014001394514c646
ibkaufma <markus.kaufmann@stud.hslu.ch>
2018-09-22 20:09:34 +0200

    Implemented generic version

--- SW01/Java_A3/src/ch/hslu/pcp/sw01/a3/interfaces/Linkable.java
+++ SW01/Java_A3/src/ch/hslu/pcp/sw01/a3/interfaces/Linkable.java
@@ -2,6 +2,5 @@ package ch.hslu.pcp.sw01.a3.interfaces;
 
 public interface Linkable<T> {
     T getNext();
-
     void setNext(T next);
 }
--- SW01/Java_A3/src/ch/hslu/pcp/sw01/a3/interfaces/Value.java
+++ SW01/Java_A3/src/ch/hslu/pcp/sw01/a3/interfaces/Value.java
@@ -2,6 +2,5 @@ package ch.hslu.pcp.sw01.a3.interfaces;
 
 public interface Value<T> {
     void setValue(T value);
-
     T getValue();
 }
